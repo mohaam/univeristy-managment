513b44ca882467be8de7268a710202daa4ed9a1e
mohamed ahmed <mohamedahmedelsaid99@gmail.com>
2018-03-25 08:34:32 -0700

    add ajax and problem solved

---
+++ static/js/courses-control.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelectorAll("#del").forEach(btn =>{
+        btn.onclick =()=> {
+             request = new XMLHttpRequest();
+             request.open('POST','/courses_control_ajax');
+            request.onload=function()
+            {
+                document.location = document.location;
+               
+            }
+            data_send = new FormData();
+            data_send.append('do','delete');
+            data_send.append('id',btn.dataset.id);
+            request.send(data_send);
+        }
+    });
+        document.querySelectorAll('td').forEach(cell =>{
+        cell.onclick =()=> {
+            newinfo = prompt('Enter the new information');
+            if(newinfo !=null)
+                {
+                    request = new XMLHttpRequest();
+                     request.open('POST','/courses_control_ajax');
+                    request.onload=function()
+                    {
+                        document.location = document.location;
+
+                    }
+                    data_send = new FormData();
+                    data_send.append('do','edit');
+                    data_send.append('id',cell.dataset.id);
+                    data_send.append('what',cell.dataset.info);
+                    data_send.append('newinfo',newinfo);
+
+                    request.send(data_send);
+                }
+        }
+    });
+
+});
